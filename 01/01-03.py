347d275d6f12f70e82a8ff980ca46099a2bc9378
Carlos Garcia <cgarciams@hotmail.com>
2026-09-11 12:04:43 +0200

    01-03

--- 01/01-03.py
+++ 01/01-03.py
@@ -11,11 +11,11 @@ print(f"\nHello world!")
 print(f"Python process running with process id: {os.getpid()}")
 
 hello_thread = threading.Thread(target=hello_from_thread)
-hello_thread.start()
+hello_thread.start()                                                # Start running hello_thread
  
 total_threads = threading.active_count()
 thread_name = threading.current_thread().name
 print(f"Python is currently running {total_threads} thread(s)")
 print(f"The current thread is {thread_name}\n")
  
-hello_thread.join()
+hello_thread.join()                                                 # Wait until hello_thread has finished
