e88db7818cc68dcd764cacb58f158333f5c81889
Carlos Garcia <cgarciams@hotmail.com>
2026-09-11 09:14:10 +0200

    01-01

--- 01/01-01.py
+++ 01/01-01.py
@@ -6,22 +6,22 @@ import urllib3
 import requests
 from rich.console import Console
 
-URL = "https://www.roche.com"
+URL = "https://www.python.org"
 FILE = "headers.txt"
 
-console = Console()
-
-# Suppress the obnoxious warning that prints when you disable verification
+# Suppress the warning that prints when you disable verification
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Sends the HTTP GET request to the web server and get the whole response in an object 
-#response = requests.get('https://www.example.com', verify=False)
-
+console = Console()
 start_time = time.time()
 console.print(f"\nLoading {URL}", style="gold1", highlight=False)
+# Sends the HTTP GET request to the web server and get the whole response in an object 
 response = requests.get(URL, verify=False)
 
-# response = requests.get('https://www.elpais.es', verify=False)
+# from response.headers, I get the size of the HTTP reply 
+content_length_bytes = int(response.headers.get('Content-Length', 0))
+content_lenght_kb = content_length_bytes / 1024
+print(f"Payload size: {content_lenght_kb:.2f} KB")
 
 # response.headers is a dictionary and items() is a method of that dictionary
 # items() returns a 'dictionary view' which is easily iterable
@@ -34,10 +34,11 @@ headers = [f"{key}: {header}" for key, header in items]
 formatted_headers = '\n'.join(headers)
 
 # Opens a file for writing and writes the string
-console.print(f"Writing {FILE}", style="gold1", highlight=False)
+print(f"Writing {FILE}")
 with open(FILE, 'w') as file:
     file.write(formatted_headers)      
 
+# Show time
 end_time = time.time()
 execution_time_seconds = (end_time - start_time)
 console.print(f"Time: {execution_time_seconds:.2f} seconds\n", style="cyan", highlight=False)
