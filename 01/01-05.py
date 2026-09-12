c3c7180cda26a3ab04ac203270550b8f996c3176
Carlos Garcia <cgarciams@hotmail.com>
2026-09-12 08:28:45 +0200

    01-05

---
+++ 01/01-05.py
@@ -0,0 +1,34 @@
+# Generating and timing the Fibonacci sequence
+
+import time
+ 
+# Calculates and prints a specific number in the Fibonacci sequence
+# Uses nested functions and recursion
+def print_fib(number):                         # Outer function: takes the input and prints the output
+    def fib(n):                                # Nested function: can only be called from print_fib()
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)    # Recursion: the function definition calls himself
+ 
+    print(f"Number {number} in the Fibonacci sequence is {fib(number)}")
+ 
+ 
+def fibonacci_no_threading():
+    print_fib(40)
+    print_fib(41)
+ 
+ 
+print()
+for n in range(1, 7):
+    print_fib(n)
+
+start_time = time.time()
+fibonacci_no_threading()
+ 
+# Show time
+end_time = time.time()
+execution_time_seconds = (end_time - start_time)
+print(f"Time: {execution_time_seconds:.2f} seconds\n")
