713e83b2b2d9f3b0f18892acd62a0a1b829f1b16
Carlos Garcia <cgarciams@hotmail.com>
2026-09-12 20:24:20 +0200

    01-05 and 01-06

--- 01/01-06.py
+++ 01/01-06.py
@@ -1,8 +1,10 @@
 # Generating and timing the Fibonacci sequence
-# Single-thread Python application
+# Multithreaded Python application
 # While it's running open $ htop to see how one core will be 100% busy
+# Also the Ubuntu scheduled can assign each thread to a core, but the GIL will make that only one is active at a time (50% / 50%)
 
 import time
+import threading
  
 # Calculates and prints a specific number in the Fibonacci sequence
 # Uses nested functions and recursion
@@ -16,20 +18,22 @@ def print_fib(number):                         # Outer function: takes the inpu
             return fib(n - 1) + fib(n - 2)    # Recursion: the function definition calls himself
  
     print(f"Number {number} in the Fibonacci sequence is {fib(number)}")
- 
- 
-def fibonacci_no_threading():
-    print_fib(40)
-    print_fib(41)
- 
- 
-print()
-for n in range(1, 7):
-    print_fib(n)
 
+     
+def fibonacci_with_threads():
+    fortieth_thread = threading.Thread(target=print_fib, args=(40,))
+    forty_first_thread = threading.Thread(target=print_fib, args=(41,))
+ 
+    fortieth_thread.start()
+    forty_first_thread.start()
+ 
+    fortieth_thread.join()
+    forty_first_thread.join()
+ 
+ 
 print("\nStarting 40 and 41")
 start_time = time.time()
-fibonacci_no_threading()
+fibonacci_with_threads()
  
 # Show time
 end_time = time.time()
