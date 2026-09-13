b44a1962a99506f66b15a966b8902e8ecd4cb4ea
Carlos Garcia <cgarciams@hotmail.com>
2026-09-13 08:01:25 +0200

    01-07

---
+++ 01/01-07.py
@@ -0,0 +1,82 @@
+# Generating and timing the Fibonacci sequence
+# Multiprocess Python application
+
+# While it's running, open $ htop 
+# The OS scheduler will assign its own memory space and one core to each process
+# The GIL is not an issue as they are processes, each one runs Python bytecode at the same time
+# Result: parallelism and speedup, the app will take just the time of the longest process, not the sum of all them
+
+import time
+import multiprocessing
+ 
+# Calculates and prints a specific number in the Fibonacci sequence
+# Uses nested functions and recursion
+def print_fib(number):                         # Outer function: takes the input and prints the output
+    def fib(n):                                # Nested function: can only be called from print_fib()
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)    # Recursion: the function definition calls itself
+ 
+    print(f"Number {number} in the Fibonacci sequence is {fib(number)}")
+
+     
+def fibonacci_process_forty_code():
+    # Defines the work for this process
+    print_fib(50)
+ 
+def fibonacci_process_fortyone_code():
+    # Defines the work for this process
+    print_fib(51)
+
+def fibonacci_process_fortytwo_code():
+    # Defines the work for this process
+    print_fib(52)
+ 
+def fibonacci_process_fortythree_code():
+    # Defines the work for this process
+    print_fib(53) 
+
+def fibonacci_process_fortyfour_code():
+    # Defines the work for this process
+    print_fib(54) 
+
+
+
+if __name__ == '__main__':                                   # Main is best practice in normal scripts but mandatory in multiprocessing scripts
+
+    print("\nStarting Fibonacci parallel calculations")
+    start_time = time.time()
+
+    fibonacci_process_forty = multiprocessing.Process(target=fibonacci_process_forty_code)
+    fibonacci_process_forty.start()
+    
+    fibonacci_process_fortyone = multiprocessing.Process(target=fibonacci_process_fortyone_code)
+    fibonacci_process_fortyone.start()
+
+    fibonacci_process_fortytwo = multiprocessing.Process(target=fibonacci_process_fortytwo_code)
+    fibonacci_process_fortytwo.start()
+        
+    fibonacci_process_fortythree = multiprocessing.Process(target=fibonacci_process_fortythree_code)
+    fibonacci_process_fortythree.start()
+
+    fibonacci_process_fortyfour = multiprocessing.Process(target=fibonacci_process_fortyfour_code)
+    fibonacci_process_fortyfour.start()
+    
+    print(f"\nHello from parent process")
+
+    # Wait until the child processes have finished before closing the parent process
+    fibonacci_process_forty.join()                         
+    fibonacci_process_fortyone.join() 
+    fibonacci_process_fortytwo.join() 
+    fibonacci_process_fortythree.join()
+    fibonacci_process_fortyfour.join() 
+ 
+    # Show time
+    end_time = time.time()
+    execution_time_seconds = (end_time - start_time)
+    print(f"Time: {execution_time_seconds:.2f} seconds\n")
+
+    
