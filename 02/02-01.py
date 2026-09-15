764c3e355cef7e5cb9096eeda502c7debe537a75
Carlos Garcia <cgarciams@hotmail.com>
2026-09-15 13:03:21 +0200

    02-01

--- 02/02-01.py
+++ 02/02-01.py
@@ -1,5 +1,7 @@
 # Asyncio: Coroutines
 
+import asyncio
+
 async def coroutine_greet():
     print("Hello, world")
 
@@ -20,9 +22,17 @@ greet()
 result = add_one(1)
 print(f"Result 1+1: {result}")
 
+
 # Try to run the coroutine (it won't work, as coroutines are not run just by entering the name as if they were normal functions)
 # The code inside will not run and it will just return a coroutine object
 coroutine_result = coroutine_add_one(1)
+print(f"Coroutine result 1+1: {coroutine_result}")
+
+# Tell Python to safely close it so it doesn't throw a warning when deleted
+coroutine_result.close()
+
+# Now I run the coroutine using asyncio.run()
+coroutine_result = asyncio.run(coroutine_add_one(1))
 print(f"Coroutine result 1+1: {coroutine_result}")
 
 print()
