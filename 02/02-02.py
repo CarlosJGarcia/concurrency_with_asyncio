1f9f6ca1c16cf9d111fd1f4c7b21b8a79739798a
Carlos Garcia <cgarciams@hotmail.com>
2026-09-17 07:10:13 +0200

    02-03

--- 02/02-02.py
+++ 02/02-02.py
@@ -1,11 +1,14 @@
+# This script shows the basic syntax of coroutines
 import asyncio
  
 async def add_one(number):
     return number + 1
  
  
+# Couroutine that orchestrates the program
 async def main():
-    one_plus_one = await add_one(1)     # Runs add_one() and pauses the coroutine where it is contained until add_one() finishes and returns a result
+    # Run add_one(1) and add_one(2) one after the other and get the result
+    one_plus_one = await add_one(1)     # Runs add_one() and pauses the coroutine where it is contained (main()) until add_one() finishes and returns a result
     two_plus_one = await add_one(2)     # Idem  
 
     print()
---
+++ 02/02-03.py
@@ -0,0 +1,14 @@
+# Program that sleeps for 1 second and then prints a "hello, world" message
+
+import asyncio
+ 
+async def message():
+    await asyncio.sleep(5)     # Simulates a IO-Bound task, for example a LLM inference request or a SQL query that takes 1 second             
+    return "Hello, world"
+ 
+async def main():
+    print()
+    msg = await message()
+    print(f"{msg}\n")
+ 
+asyncio.run(main())
