8ae9988da0f451a1a6db069f7d1f69283a914156
Carlos Garcia <cgarciams@hotmail.com>
2026-09-17 07:30:44 +0200

    Delay

--- 02/02-03.py
+++ 02/02-03.py
@@ -5,7 +5,8 @@ import asyncio
 async def message():
     await asyncio.sleep(5)     # Simulates a IO-Bound task, for example a LLM inference request or a SQL query that takes 1 second             
     return "Hello, world"
- 
+
+
 async def main():
     print()
     msg = await message()
